c5c48627b2d26b141d94fc14e79af13de617fc23
alejocbs <cbueno@umd.edu>
2020-04-30 21:38:13 -0400

    Data Working

--- main.py
+++ main.py
@@ -10,45 +10,14 @@ import numpy as np
 
 
 def UndistortImage(image, LUT):
+    print(image.shape)
+    print(LUT.shape)
     reshaped_lut = LUT[:, 1::-1].T.reshape((2, image.shape[0], image.shape[1]))
-    undistorted = np.rollaxis(np.array([interp2(image[:, :, channel], reshaped_lut, order=1) for channel in range(0, image.shape[2])]), 0, 3).shape
+    undistorted = np.rollaxis(np.array([interp2(image[:, :, channel], reshaped_lut, order=1) for channel in range(0, image.shape[2])]), 0, 3)
 
     return undistorted.astype(image.dtype)
 
 def ReadCameraModel():
-
-# ReadCameraModel - load camera intrisics and undistortion LUT from disk
-#
-#
-# INPUTS:
-#   image_dir: directory containing images for which camera model is required
-#   models_dir: directory containing camera models
-#
-# OUTPUTS:
-#   fx: horizontal focal length in pixels
-#   fy: vertical focal length in pixels
-#   cx: horizontal principal point in pixels
-#   cy: vertical principal point in pixels
-#   G_camera_image: transform that maps from image coordinates to the base
-#     frame of the camera. For monocular cameras, this is simply a rotation.
-#     For stereo camera, this is a rotation and a translation to the left-most
-#     lense.
-#   LUT: undistortion lookup table. For an image of size w x h, LUT will be an
-#     array of size [w x h, 2], with a (u,v) pair for each pixel. Maps pixels
-#     in the undistorted image to pixels in the distorted image
-################################################################################
-#
-# Copyright (c) 2019 University of Maryland
-# Authors:
-#  Kanishka Ganguly (kganguly@cs.umd.edu)
-#
-# This work is licensed under the Creative Commons
-# Attribution-NonCommercial-ShareAlike 4.0 International License.
-# To view a copy of this license, visit
-# http://creativecommons.org/licenses/by-nc-sa/4.0/ or send a letter to
-# Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
-#
-################################################################################
 
     models_dir="Oxford_dataset/model"
 
@@ -78,9 +47,10 @@ files=loadImages("Oxford_dataset/stereo/centre")
 images = []
 for file in files:
     images=cv2.imread(file, cv2.IMREAD_UNCHANGED)
-    print(images)
     cv2.imshow("video", images)
     cv2.waitKey(10)
     color_image = cv2.cvtColor(images, cv2.COLOR_BayerGR2BGR)
+    cv2.imshow("Original", color_image)
+    cv2.waitKey(10)
     fx, fy, cx, cy, G_camera_image, LUT = ReadCameraModel()
-    undistorted_image = UndistortImage(images, LUT)
+    undistorted_image = UndistortImage(color_image, LUT)
